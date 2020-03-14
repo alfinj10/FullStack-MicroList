b1ab9f0d7847a6e915928f5889d1e9c148a274fa
Alfin Jose <alfin.jose@publicissapient.com>
2020-03-14 18:07:58 +0000

    Completed front end - ready to deploy to heroku

--- server/index.js
+++ server/index.js
@@ -5,12 +5,19 @@ const cors = require('cors');
 const app = express();
 
 //Middleware 
-
 app.use(bodyParser.json());
 app.use(cors());
 
 const posts = require('./routes/api/posts');
 app.use('/api/posts', posts);
+
+//Handle production 
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(__dirname + '/public/'));
+    //Handle SPA
+    app.get(/.*/, (req,res) => res.sendFile(__dirname + '/public/index,html'));
+}
 
 const port = process.env.PORT || 5000;
 
